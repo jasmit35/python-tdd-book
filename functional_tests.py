1ef7d7904f22b9f6648a138f93b3fbd4a96138aa
Jeff Smith <>
2023-02-06 19:28:27 -0600

    Refactor home page view to use a template.

--- functional_tests.py
+++ functional_tests.py
@@ -16,6 +16,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
@@ -29,6 +30,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,5 +18,5 @@ from django.conf.urls import url
 from lists import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
+    url(r'^$', views.home_page, name='home.html'),
 ]
